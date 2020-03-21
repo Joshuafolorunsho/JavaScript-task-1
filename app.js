fb149d2aad2561b77aa7288d2c38de0d2990aed2
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-03-21 14:44:16 +0100

    description of the task

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+/*
+Description
+
+Task: 
+
+
+Store your name in a varibale. 
+
+Store your courses in an array. 
+
+Display your name and courses and do one of the following: 
+
+
+1. If the number of courses you are doing is an odd number, find and display all odd numbers from 1-200 (inclusive)
+2. If the number of courses you are doing is an even number, find and display all even numbers from 1-200 (inclusive)
+*/
+
+// Solution
 const name = 'Joshua Folorunsho';
 
 const courses = ['HTML', 'CSS', 'JS'];
